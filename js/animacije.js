ec1c38d58fc9af7c8b483eb0b4559f938d03808f
djoledjordjevic00 <djoledjordjevic00@gmail.com>
2025-09-24 11:17:28 +0200

    Add files via upload

---
+++ js/animacije.js
@@ -0,0 +1,20 @@
+
+const faders = document.querySelectorAll('.fade-in, .slide-left, .slide-right');
+
+  const appearOptions = {
+    threshold: 0.2,
+    rootMargin: "0px 0px -50px 0px"
+  };
+
+  const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+
